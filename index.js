3356a42b8a729ff6629c66d8cee200773d5a3f3e
Brody Losh <brodylosh@gmail.com>
2022-04-15 15:35:20 -0400

    commit tree 338a788d602ab0074cdb8b8794114d94d8b43527

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+const submitData = (userName, userEmail) => {
+    const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify( {
+            name: userName,
+            email: userEmail,
+        })
+    }
+    
+    return fetch('http://localhost:3000/users', configurationObject)
+    .then(resp => resp.json())
+    .then(obj => document.body.innerHTML = (obj.id))
+    .catch(error => document.body.innerHTML = (error.message))
+}
+
+submitData('brody', 'brodylosh@gmail.com');
